72f7c2f33f4dc580e65fc7a3b6f0d594ad65c358
Kiaizen <kiaizenx@gmail.com>
2024-06-17 21:56:45 -0300

    Subindo na vercel 4

--- api/index.js
+++ api/index.js
@@ -1,14 +1,22 @@
 const path = require('path');
 const jsonServer = require('json-server');
-const server = jsonServer.create();
+const express = require('express');
+const app = express();
+
+// Middlewares do json-server
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
-server.use(jsonServer.defaults({
-  static: path.join(__dirname, '../public')
-}));
-server.use(router);
-server.listen(3000, () => {
+// Usar middlewares do json-server
+app.use(middlewares);
+
+// Servir arquivos estáticos da pasta 'public'
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Usar o router do json-server
+app.use('/api', router);
+
+// Iniciar o servidor
+app.listen(3000, () => {
   console.log('JSON Server is running');
 });

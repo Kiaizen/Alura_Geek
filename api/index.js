0641af041369e90bc888e3eb54f7ccf67d509795
Kiaizen <kiaizenx@gmail.com>
2024-06-17 21:50:17 -0300

    Subindo na vercel 3

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
+const path = require('path');
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use(jsonServer.defaults({
+  static: path.join(__dirname, '../public')
+}));
 server.use(router);
 server.listen(3000, () => {
   console.log('JSON Server is running');
